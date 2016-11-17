1b2d5bb952fe57b8e8eafcdda7e4f03f6801f658
Martin Söderstrand <martinsoderstrand96@hotmail.com>
2016-11-17 18:26:09 +0100

    The foundation of the android and webservice project created

---
+++ android/Mellow/app/src/main/java/com/mellow/FlowActivity.java
@@ -0,0 +1,15 @@
+package com.mellow;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.mellow.mellow.R;
+
+public class FlowActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flow);
+    }
+}
---
+++ webservice/MellowService/src/test/java/com/mellow/entity/TestUser.java
@@ -0,0 +1,7 @@
+package com.mellow.entity;
+
+/**
+ * Created by Martin on 2016-11-17.
+ */
+public class TestUser {
+}
