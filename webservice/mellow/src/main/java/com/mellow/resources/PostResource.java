408f5733223bbc9e1d4b6e2fdb13ee78ada626d7
Martin Söderstrand <Martinsoderstrand96@hotmail.com>
2016-12-14 09:39:22 +0100

    getAllPosts now returns a list.

--- webservice/mellow/src/main/java/com/mellow/resources/PostResource.java
+++ webservice/mellow/src/main/java/com/mellow/resources/PostResource.java
@@ -2,6 +2,7 @@ package com.mellow.resources;
 
 import com.mellow.model.Post;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -11,16 +12,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Path("posts")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class PostResource {
 
     @GET
-    @Produces(MediaType.APPLICATION_JSON)
     public Response getAllPosts(){
         List<Post> posts = new ArrayList<>();
         posts.add(new Post("This is just a temp text!", "Test1", "Testsson1"));
         posts.add(new Post("This is a longer temp text to show how it will look in the future when someone type something longer!", "Test2", "Testsson2"));
         posts.add(new Post("This is just a temp text to show how it will look in the future3!", "Test3", "Testsson3"));
         posts.add(new Post("This is just a temp text to show how it will look in the future4!", "Test4", "Testsson4"));
-        return Response.ok(new Post("This is just a temp text!", "Test1", "Testsson1")).build();
+        return Response.ok(posts).build();
     }
 }
